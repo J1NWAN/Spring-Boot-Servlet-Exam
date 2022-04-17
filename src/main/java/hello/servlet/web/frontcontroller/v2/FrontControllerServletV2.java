0059b7d584ff9abcea812d13f9b23cbb5b6a0595
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2022-04-17 11:10:31 +0900

    공통으로 사용되는 View를 분리하여 FrontControllerV2를 이용하여 렌더링

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -1,9 +1,9 @@
 package hello.servlet.web.frontcontroller.v2;
 
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v2.Controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v2.Controller.MemberListControllerV2;
-import hello.servlet.web.frontcontroller.v2.Controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
